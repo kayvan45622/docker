5de13e34b8f407cfacfab9cfda6405e777404fa5
Kevin khalafi <34110373+kayvan45622@users.noreply.github.com>
2021-11-12 20:51:54 -0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,19 @@
+const http = require('http');
+const os = require('os');
+
+function sayHello(name)
+{
+  console.log('Hello ' + name)
+}
+
+console.log("Kevin Server Starting ...");
+
+var handler = function(request, response){
+    console.log(`Request recived from: ${request.connection.remoteAddress}`);
+    response.writeHead(200);
+    response.end(`You are here!!!${os.hostname()}\n`);
+};
+
+var www= http.createServer(handler);
+
+www.listen(8080);
